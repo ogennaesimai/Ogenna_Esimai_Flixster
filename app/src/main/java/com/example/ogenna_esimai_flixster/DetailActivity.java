7d1b85e70888d5af2a31ff494653e4177592e863
Ogenna Esimai <ogenna.esimai@mavs.uta.edu>
2020-10-03 00:13:13 -0500

    Introduced a details page and updated app to render trailers with a third party YouTube library.

---
+++ app/src/main/java/com/example/ogenna_esimai_flixster/DetailActivity.java
@@ -0,0 +1,101 @@
+package com.example.ogenna_esimai_flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.ogenna_esimai_flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+//public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_API_KEY = "AIzaSyC0MZHJVoZSXcaiq6mUkmdeO-bFpxCzHV0";
+    //public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/209112/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+    YouTubePlayerView youTubePlayerView;
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.player);
+
+        //now using Parcel. title unneeded
+        //String title = getIntent().getStringExtra("title");
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        //after getting movieId, we can change this hard coded part to use the getter
+        //client.get(String.format(VIDEOS_URL, 209112), new JsonHttpResponseHandler()
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    if (results.length() == 0) {
+                        return;
+                    }
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    //all of youTubePlayerView method gets moved into the body of this new method
+                    initializeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    Log.e("DetailActivity", "Failed to parse JSON", e);
+                    //e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+    private void initializeYoutube(final String youtubeKey) {
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+                // this is where you can cue video, play video, etc.
+                // Instead of hard coding, pass in the youtubeKey
+                //youTubePlayer.cueVideo("5xVh-7ywKpE");
+                youTubePlayer.cueVideo(youtubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationFailure");
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/ogenna_esimai_flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/ogenna_esimai_flixster/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.ogenna_esimai_flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.ogenna_esimai_flixster.DetailActivity;
 import com.example.ogenna_esimai_flixster.R;
 import com.example.ogenna_esimai_flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -56,7 +62,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         //define member variables for each view in the viewholder(row to be rendered)
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -66,9 +74,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -87,6 +96,21 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             //backdrop_path
             Glide.with(context).load(imageUrl).into(ivPoster);
 
+            //1. register click listener on the whole row (as opposed to only on Title)- so changed
+            // tvTitle to container
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //2. navigate to a new activity on tap
+                    //get rid of toast, bcos now unneeded, will use intent
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //tedious to enter components individually. Enter Parcelable library
+                    //i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
